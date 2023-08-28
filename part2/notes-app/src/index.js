2396da70facc4a3e8b27f37e84913bc112031a99
lishuweb <maharjanlishu@gmail.com>
2023-08-28 08:28:50 +0545

    feat: updated

--- part2/notes-app/src/index.js
+++ part2/notes-app/src/index.js
@@ -1,8 +1,15 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { createRoot } from "react-dom/client";
 
-import App from './App'
+import App from './App';
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-<App />
-)
+let notes = [
+    { id: 1, content: "note 1", important: true},
+    { id: 1, content: "note 1", important: true},
+    { id: 1, content: "note 1", important: true},
+];
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App notes={notes} />);
--- part3/notes-server/utils/config.js
+++ part3/notes-server/utils/config.js
@@ -2,7 +2,9 @@ require("dotenv").config();
 
 const url = process.env.NODE_ENV === "test" 
     ? process.env.TEST_MONGODB_URI
-    : process.env.MONGO_URI;
+    : process.env.MONGODB_URI;
+
+    // console.log(process.env.MONGODB_URI, "hello");
 
 const PORT = process.env.PORT;
 module.exports = { url, PORT };
