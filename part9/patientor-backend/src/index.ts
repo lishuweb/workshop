1c24e5403ac4031f8e7e6fe13f66b00222cf3e88
Lishu Maharjan <maharjanlishu@gmail.com>
2023-10-09 21:55:35 +0545

    feat: part9eupdated

---
+++ part9/patientor-backend/src/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+// import cors from "cors";
+
+const app = express();
+
+app.get("/ping", (_req, res) => {
+    res.send("pong");
+}); 
+
+
+const PORT = 3001;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
